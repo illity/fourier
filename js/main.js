a6ae21a990eba1e9c96b60f85d45c88f1cb66525
illity <arthursbdantas@gmail.com>
2023-05-17 20:10:45 -0300

    Add fourier animation in the center of the screen

--- js/main.js
+++ js/main.js
@@ -22,3 +22,41 @@ class Complex {
         return new Complex(this.r**a, this.phi*a, true);
     }
 }
+
+const canvas = document.getElementById('canva')
+const ctx = canva.getContext('2d')
+
+const width = canvas.width
+const height = canvas.height
+
+ctx.translate(width/2, height/2)
+var pos = new Complex(0, 0)
+
+ctx.strokeStyle = 'red';
+
+var t = 0
+
+function draw() {
+    const f = 128;
+    const k = 3
+    const freq = document.getElementById('frequency').value/k;
+    console.log()
+    if (t > f * freq * k) {
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        ctx.clearRect(0, 0, width, height);
+        ctx.beginPath();
+        ctx.translate(width/2, height/2)
+        t = 0;
+    }
+    const g = (t) => {return Math.cos(2*Math.PI*t/f * 1/freq)}
+    if (t == 0)
+    ctx.moveTo(200*g(t)*Math.cos(-2*Math.PI*t/f),
+               200*g(t)*Math.sin(-2*Math.PI*t/f));
+    ctx.lineTo(200*g(t)*Math.cos(-2*Math.PI*t/f),
+               200*g(t)*Math.sin(-2*Math.PI*t/f));
+    ctx.stroke();
+    t += 4 * freq;
+    window.requestAnimationFrame(draw);
+}
+
+window.requestAnimationFrame(draw);
